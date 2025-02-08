6cc7d5baea3efaaf5633ee6ed112303d6b76a729
Scott <scottld@stud.ntnu.no>
2025-02-08 15:37:37 +0100

    Removed unecessary files

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <asio.hpp>
 #include "include/TLSServer.h"
-#include "include/TLSClient.h"
 
 int main() {
     asio::io_context event_loop(1);
 
-    TLSServer echo_server;
-    co_spawn(event_loop, echo_server.start(), asio::detached);
-
-    TLSClient echo_client;
-    co_spawn(event_loop, echo_client.start(), asio::detached);
+    TLSServer TLS_server;
+    co_spawn(event_loop, TLS_server.start(), asio::detached);
 
     event_loop.run();
 }
